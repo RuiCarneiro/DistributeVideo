370ada4616155c1c2a172337f76c1f7b6c3b7541
RuiCarneiro <rui.nelson@hotmail.com>
2016-08-15 05:10:42 +0100

    More usable shell script

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+#Configuration here
+
+EMAIL=1                 # Change to zero to disable. Requires working MTA
+EMAIL_SUBJECT="Video files distribution"  #Keep between quotation marks
+EMAIL_RECIPIENT=root    # Change to your e-mail address if needed.
+
+#End of configuration
+
 
 DIRNAME=$(dirname $0)
 cd $DIRNAME
@@ -6,10 +15,11 @@ INSTALL=$(pwd)
 
 runfile=/tmp/run$$.txt
 
-echo "Distribution of video files" > $runfile
 date >> $runfile
 python3 $INSTALL/__init__.py | tee -a $runfile
 
-cat $runfile | mail -s "Video Distribution" root
+if [ $EMAIL = "1" ]; then
+	cat $runfile | mail -s "$EMAIL_SUBJECT" $EMAIL_RECIPIENT
+fi
 
 rm $runfile
