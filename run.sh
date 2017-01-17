4838022bc898ae3852a24dd3d85e15a9051986e6
RuiCarneiro <rui.nelson@hotmail.com>
2017-01-17 13:47:14 +0000

    Only send emails if not empty

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #Configuration here
 
-EMAIL=1                 # Change to zero to disable. Requires working MTA
+EMAIL="on"               # Change to off to disable. Requires working MTA
 EMAIL_SUBJECT="Video files distribution"  #Keep between quotation marks
 EMAIL_RECIPIENT=root    # Change to your e-mail address if needed.
 
@@ -18,8 +18,11 @@ runfile=/tmp/run$$.txt
 date >> $runfile
 python3 $INSTALL/__init__.py | tee -a $runfile
 
-if [ $EMAIL = "1" ]; then
-	cat $runfile | mail -s "$EMAIL_SUBJECT" $EMAIL_RECIPIENT
+if [ $EMAIL = "on" ]; then
+	lines=$(wc -l $runfile | awk '{print $1}')
+	if [ $lines > 1 ]; then
+		cat $runfile | mail -s "$EMAIL_SUBJECT" $EMAIL_RECIPIENT
+	fi
 fi
 
 rm $runfile
